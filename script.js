80931c9326347026e3f5a78ff28bbfd3b1311743
ingcesaroman <ing.cesaroman@gmail.com>
2020-04-05 14:03:35 -0500

    Agregar basicos del Mundo JS

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("script listo");
+var nombreUsuario = prompt("Cual es tu nombre? ")
+var apellidoUsiario = prompt("Cual es tu apellido? ")
+var edadUsuario = prompt("Cual es tu edad?")
+console.log("Tu nombre es " + nombreUsuario);
+console.log('Tu nombre completo es ' + nombreUsuario + " " + apellidoUsiario);
+console.log("Tienes " + edadUsuario + "de edad");
+alert('Tu nombre completo es ' + nombreUsuario + " " + apellidoUsiario + " y tienes " + edadUsuario + " de edad");
